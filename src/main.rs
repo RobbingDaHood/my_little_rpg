2029f7d6320eff7d80882b575e6d9055f100a10b
RobbingDaHood <he@nykredit.dk>
2022-04-17 20:42:19 +0200

    added Listener

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,15 @@
-use std::net::{TcpListener, TcpStream};
-use std::time::Duration;
+
 use crate::game::Game;
-use std::io::Read;
+use crate::tcp_listener::Listener;
 
 mod game;
 mod attack_types;
 mod crafting_materials;
 mod place;
 mod command_state;
+mod tcp_listener;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:1337").unwrap();
-
-
-    println!("Game is ready and listening on: 0.0.0.0:1337");
-
-    let mut game = Game::new(Vec::new());
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                game = handle_request(&mut stream, game)
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
-        }
-    }
+    Listener::new().listen()
 }
 
-fn handle_request(stream: &mut TcpStream, game: Game) -> Game {
-    let mut buffer = [0; 1024];
-    if let Err(e) = stream.set_read_timeout(Some(Duration::from_secs(1))) {
-        println!("Got error from setting timeout on reading tcp input, aborting: {}", e);
-        return game;
-    }
-    let buffer_size = match stream.read(&mut buffer) {
-        Ok(buffer_size_value) => { buffer_size_value }
-        Err(e) => {
-            println!("Got error from reading command, aborting: {}", e);
-            return game;
-        }
-    };
-    let command = &buffer[..buffer_size];
-    let command_as_string = String::from_utf8(command.to_vec()).unwrap();
-    println!("Received request with following command: {}", command_as_string);
-
-
-
-    game
-}
---
+++ src/tcp_listener.rs
@@ -0,0 +1,58 @@
+use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use serde_json::json;
+use crate::Game;
+use std::io::Read;
+
+pub struct Listener {
+    tcp_listener: TcpListener
+}
+
+impl Listener {
+    pub fn new() -> Self {
+        Self { tcp_listener: TcpListener::bind("0.0.0.0:1337").unwrap() }
+    }
+
+    pub fn listen(&self) {
+        let mut game = Game::new(Vec::new());
+        println!("Game is ready and listening on: 0.0.0.0:1337");
+
+        for stream in self.tcp_listener.incoming() {
+            match stream {
+                Ok(mut stream) => {
+                    game = self.handle_request(&mut stream, game);
+                }
+                Err(e) => {
+                    println!("Error: {}", e);
+                    /* connection failed */
+                }
+            }
+        }
+    }
+
+    fn handle_request(&self, stream: &mut TcpStream, game: Game) -> Game {
+        let mut buffer = [0; 1024];
+        if let Err(e) = stream.set_read_timeout(Some(Duration::from_secs(1))) {
+            println!("Got error from setting timeout on reading tcp input, aborting: {}", e);
+            return game;
+        }
+        let buffer_size = match stream.read(&mut buffer) {
+            Ok(buffer_size_value) => { buffer_size_value }
+            Err(e) => {
+                println!("Got error from reading command, aborting: {}", e);
+                return game;
+            }
+        };
+        let command = &buffer[..buffer_size];
+        let command_as_string = String::from_utf8(command.to_vec()).unwrap();
+        println!("Received request with following command: {}", command_as_string);
+
+        if command_as_string == "State" {
+            println!("{}", json!(game));
+        }
+
+
+        game
+    }
+}
+
