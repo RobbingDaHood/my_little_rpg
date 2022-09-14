ed291c84539417189e7f512b2f34b60248cbe74b
RobbingDaHood <he@nykredit.dk>
2022-09-14 21:41:49 +0200

    Improved decode_hex a bit, not done yet.

--- src/hex_encoder.rs
+++ src/hex_encoder.rs
@@ -1,9 +1,23 @@
-use std::{fmt::Write, num::ParseIntError};
+use std::fmt::Write;
+use std::num::ParseIntError;
+use std::ops::Not;
 
-pub fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, String> {
+    match s.chars().find(is_not_ascii_hexdigit) {
+        Some(offending_char) => Err(format!("{} is not a hexdigit!", offending_char)),
+        None => decode_hex_unsafe(&s)
+    }
+}
+
+const fn is_not_ascii_hexdigit(c: &char) -> bool {
+    !char::is_ascii_hexdigit(c)
+}
+
+fn decode_hex_unsafe(s: &&str) -> Result<Vec<u8>, String> {
     (0..s.len())
         .step_by(2)
         .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .map(|i| i.map_err(|s| s.to_string()))
         .collect()
 }
 
@@ -22,16 +36,15 @@ mod tests_int {
 
     #[test]
     fn hex_encode_decode() {
-        let data : [u8; 16] = [1; 16];
+        let data: [u8; 16] = [1; 16];
 
         let encoded = encode_hex(&data);
 
-        let decoded : [u8; 16] = decode_hex(encoded.as_str())
+        let decoded: [u8; 16] = decode_hex(encoded.as_str())
             .unwrap()
             .try_into()
             .unwrap_or_else(|v: Vec<u8>| panic!("Seed need exactly 16 comma seperated u8; It got {:?}", v));
 
-        assert_eq!(data, decoded)
-
+        assert_eq!(data, decoded);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -46,19 +46,22 @@ pub struct Settings {
     #[structopt(short, long, default_value = "1337", help = "A u16 representing the port used to listen for incoming commands.")]
     pub(crate) port: u16,
 
-    #[structopt(short, long, parse(try_from_str = parse_seed), help = "16 hexidecimal representation of the seed. Example: e66832fd2e73fec455149e08b9c08bc1")]
+    #[structopt(short, long, parse(try_from_str = parse_seed), help = "32 hexidecimal representation of the seed. Example: e66832fd2e73fec455149e08b9c08bc1")]
     pub(crate) seed: Option<[u8; 16]>,
 }
 
 fn parse_seed(src: &str) -> Result<[u8; 16], String> {
-    Ok(decode_hex(src)
-        .unwrap_or_else(|_| panic!("Could not parse the given seed {} to hexidecimals", src))
-        .try_into()
-        .unwrap_or_else(|v: Vec<u8>| panic!("Seed need exactly 16 hexidecimals; It got {:?}", v)))
+    if src.len() != 32 {
+        return Err("32 Hexidecimals as a string requires 32 chars!".to_string())
+    }
+    match decode_hex(src) {
+        Err(e) => Err(e),
+        Ok(r) =>  Ok(r.try_into().unwrap())
+    }
 }
 
 fn main() {
     let opt = Settings::from_args();
-    Listener::new(opt.port).listen(opt.seed)
+    Listener::new(opt.port).listen(opt.seed);
 }
 
