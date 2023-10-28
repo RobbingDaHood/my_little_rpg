e834bc4df6292289f43eaaba18b1f90705f20cc7
RobbingDaHood <he@nykredit.dk>
2023-10-28 17:10:09 +0200

    Removed unused code that would have generated default messages for errors.

--- src/my_little_rpg_errors.rs
+++ src/my_little_rpg_errors.rs
@@ -1,53 +1,17 @@
-use std::{fmt, fmt::Formatter};
-
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize, Eq, Hash)]
 pub enum MyError {
-    ParseCommand {
-        error_message: Box<str>,
-    },
-    Network {
-        error_message: Box<str>,
-    },
-    SaveLoad {
-        error_message: Box<str>,
-    },
-    ExecuteCommand {
-        error_message: Box<str>,
-    },
-    MoveCommand {
-        error_message: Box<str>,
-        item_report: Box<str>,
-    },
+    ParseCommand { error_message: Box<str> },
+    Network { error_message: Box<str> },
+    SaveLoad { error_message: Box<str> },
+    ExecuteCommand { error_message: Box<str> },
 }
 
 impl From<MyError> for Box<str> {
     fn from(error: MyError) -> Self {
         format!("{error:?}").into()
-    }
-}
-
-//TODO Add general help message to all errors
-
-impl fmt::Display for MyError {
-    fn fmt(
-        &self,
-        f: &mut Formatter<'_>,
-    ) -> fmt::Result {
-        match self {
-            MyError::Network { error_message } | MyError::ExecuteCommand { error_message } | MyError::SaveLoad { error_message } | MyError::ParseCommand { error_message } => {
-                write!(
-                    f,
-                    "Got the following error while trying to parse the given command: \
-                     {error_message}"
-                )
-            }
-            MyErrorKind::MoveCommand { .. } => {
-                write!(f, "{}", json!(self))
-            }
-        }
     }
 }
 
